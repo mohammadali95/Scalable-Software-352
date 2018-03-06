7b4d77c458c2781f8f7044d46872191e8452ea64
JoshuaNorris <34869105+JoshuaNorris@users.noreply.github.com>
2018-03-06 15:59:07 -0600

    Still having network issues, but have fixed all the known errors on GUi

---
+++ Project_2/Project_2/src/DataStructures/MessageReceiver.java
@@ -0,0 +1,23 @@
+package DataStructures;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class MessageReceiver {
+
+	String messages;
+
+	public MessageReceiver(String messages) {
+		this.messages = messages;
+	}
+
+	public ObservableList<String> toList() {
+		ObservableList<String> result = FXCollections.observableArrayList();
+		String[] split = messages.split("§");
+		for (int x = 0; x < split.length; x++) {
+			result.add(split[x]);
+		}
+		return result;
+	}
+
+}
--- Project_2/Project_2/src/DataStructures/MessageSender.java
+++ Project_2/Project_2/src/DataStructures/MessageSender.java
@@ -1,41 +1,26 @@
 package DataStructures;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
-
 import javafx.collections.ObservableList;
 
 public class MessageSender {
 
-	private Conversation conversation = null;
-	
-	public MessageSender(Conversation conversation) {
-		this.conversation = conversation;
+	ObservableList<String> messages;
+
+	public MessageSender(ObservableList<String> messages) {
+		this.messages = messages;
 	}
-	
-	public BufferedWriter makeTextFile() throws IOException {
-		File textfile = new File("Conversation");
-		ObservableList<String> conversationList = conversation.getMessageHistory();
-		String conversationString = "";
-		for (int x = 0; x < conversationList.size(); x++) {
-			conversationString = conversationString + conversationList.get(x) + "\n";
+
+	@Override
+	public String toString() {
+		String result = "";
+		for (int x = 0; x < messages.size(); x++) {
+			if (x == 0) {
+				result = messages.get(0);
+			} else {
+				result = result + "§" + messages.get(x);
+			}
 		}
-		OutputStreamWriter output = null; //make a try catch for this possible null pointer exception
-		try {
-			output.write(conversationString);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		output.close();
-		
-		BufferedWriter writer = new BufferedWriter(output);
-		
-		return writer;
+		return result;
 	}
 
 }
