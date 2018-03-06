5c4d8b769df34675cf9f27c07895c76abd221824
JoshuaNorris <34869105+JoshuaNorris@users.noreply.github.com>
2018-03-06 16:00:29 -0600

    Still having network issues, but have fixed all the known errors on GUi

--- Project_2/Project_2/src/Threads/UpdateGUI.java
+++ Project_2/Project_2/src/Threads/UpdateGUI.java
@@ -1,5 +1,25 @@
 package Threads;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import DataStructures.MessageReceiver;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+
 public class UpdateGUI {
+
+	Socket socket;
+
+	public UpdateGUI (Socket socket, ListView<String> listview) throws IOException {
+		BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		String messageStr = is.readLine();
+		MessageReceiver receiver = new MessageReceiver(messageStr);
+		ObservableList<String> messageList = receiver.toList();
+		listview.setItems(messageList);
+	}
 
 }
--- Project_2/Project_2/src/Threads/WaitingForConnection.java
+++ Project_2/Project_2/src/Threads/WaitingForConnection.java
@@ -3,6 +3,7 @@ package Threads;
 import java.net.*;
 
 import Network.*;
+import javafx.scene.control.ListView;
 
 import java.io.*;
 
@@ -10,20 +11,22 @@ public class WaitingForConnection extends Thread{
 	ServerSocket s;
 	SocketListener listener;
 	Socket connectingSocket;
-	
-	public WaitingForConnection(ServerSocket s) {
+
+	public WaitingForConnection(ServerSocket s, ListView<String> listview) {
 		this.s = s;
 	}
-	
+
 	@Override
 	public void run() {
 		new Thread(() -> {try {
 			connectingSocket = s.accept();
+			// run update gui here??
+			System.out.println(connectingSocket.getInetAddress());
 		} catch(IOException e) {
 			e.printStackTrace();
 		}}).start();
 	}
-	
+
 	public Socket getConnectingSocket() {
 		return connectingSocket;
 	}
