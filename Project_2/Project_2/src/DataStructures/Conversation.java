f92b19e57a301406203f6c84b243fe6d441d78ec
JoshuaNorris <34869105+JoshuaNorris@users.noreply.github.com>
2018-03-06 22:28:29 -0600

    Messaging Works! (I think)

--- Project_2/Project_2/src/DataStructures/Conversation.java
+++ Project_2/Project_2/src/DataStructures/Conversation.java
@@ -8,22 +8,22 @@ public class Conversation {
 	private String ip;
 	private String name;
 	ObservableList<String> history = FXCollections.observableArrayList();
-	
+
 	public Conversation(String ip, String name) {
 		this.ip = ip;
 		this.name = name;
 	}
-	
+
 	public String getIP() {return ip;}
-	
+
 	public String getName() {return name;}
-	
+
 	public ObservableList<String> getMessageHistory() {return history;}
-	
+
 	public void setIP(String ip) {this.ip = ip;}
-	
+
 	public void setName(String name) {this.name = name;}
-	
+
 	public void addNewMessage(String newMessage) {history.add(newMessage);}
 
 }
